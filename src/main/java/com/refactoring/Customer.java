77c62dd2a7ed4b94f61a2216cd482f2bf66b6824
Leonid Freeman <lfreeman@realnetworks.com>
2016-04-20 15:40:44 -0400

    Moving the Amount Calculation

--- src/main/java/com/refactoring/Customer.java
+++ src/main/java/com/refactoring/Customer.java
@@ -26,7 +26,6 @@ class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental rental : rentals) {
-            double thisAmount = amountFor(rental);
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
@@ -34,37 +33,12 @@ class Customer {
             }
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(rental.getCharge()) + "\n";
+            totalAmount += rental.getCharge();
         }
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
-    }
-
-    /**
-     * @return
-     */
-    double amountFor(Rental rental) {
-        double result = 0;
-        // determine amounts for each line
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2) {
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3)
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
         return result;
     }
 }
