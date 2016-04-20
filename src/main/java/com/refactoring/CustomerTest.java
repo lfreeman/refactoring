9edbbcf34903b8c5b38c85c60162afb55191326c
Leonid Freeman <lfreeman@realnetworks.com>
2016-04-20 15:01:58 -0400

    The Starting Point

---
+++ src/main/java/com/refactoring/CustomerTest.java
@@ -0,0 +1,27 @@
+package com.refactoring;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+    @Test
+    public void test() {
+        Customer customer1 = new Customer("customer1");
+        customer1.addRental(new Rental(new Movie("movie1", 0), 7));
+        customer1.addRental(new Rental(new Movie("movie2", 1), 7));
+        customer1.addRental(new Rental(new Movie("movie3", 2), 7));
+
+        String expected = "Rental Record for customer1\n" +
+                "\tmovie1\t9.5\n" +
+                "\tmovie2\t21.0\n" +
+                "\tmovie3\t7.5\n" +
+                "Amount owed is 38.0\n" +
+                "You earned 4 frequent renter points";
+        System.out.println(expected);
+        System.out.println(customer1.statement());
+        assertEquals(expected, customer1.statement());
+    }
+
+}
