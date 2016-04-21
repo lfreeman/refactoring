e5249930982c0cf94e570194314363adcf147299
Leonid Freeman <lfreeman@realnetworks.com>
2016-04-20 21:59:16 -0400

    htmlStatement

--- src/main/java/com/refactoring/Customer.java
+++ src/main/java/com/refactoring/Customer.java
@@ -34,6 +34,23 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        for (Rental rental : rentals) {
+            // show figures for this rental
+            result += rental.getMovie().getTitle() + ": " +
+                    String.valueOf(rental.getCharge()) + "<BR>\n";
+        }
+        // add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+                "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
+
+
     private double getTotalCharge() {
         return rentals.stream().mapToDouble(Rental::getCharge).sum();
     }
--- src/test/java/com/refactoring/CustomerTest.java
+++ src/test/java/com/refactoring/CustomerTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class CustomerTest {
 
     @Test
-    public void test() {
+    public void statement() {
         Customer customer1 = new Customer("customer1");
         customer1.addRental(new Rental(new Movie("movie1", 0), 7));
         customer1.addRental(new Rental(new Movie("movie2", 1), 7));
@@ -20,6 +20,22 @@ public class CustomerTest {
                 "Amount owed is 38.0\n" +
                 "You earned 4 frequent renter points";
         assertEquals(expected, customer1.statement());
+    }
+
+    @Test
+    public void htmlStatement() {
+        Customer customer1 = new Customer("customer1");
+        customer1.addRental(new Rental(new Movie("movie1", 0), 7));
+        customer1.addRental(new Rental(new Movie("movie2", 1), 7));
+        customer1.addRental(new Rental(new Movie("movie3", 2), 7));
+
+        String expected = "<H1>Rentals for <EM>customer1</EM></H1><P>\n" +
+                "movie1: 9.5<BR>\n" +
+                "movie2: 21.0<BR>\n" +
+                "movie3: 7.5<BR>\n" +
+                "<P>You owe <EM>38.0</EM><P>\n" +
+                "On this rental you earned <EM>4</EM> frequent renter points<P>";
+        assertEquals(expected, customer1.htmlStatement());
     }
 
 }
