2c3005a8eb2c1484138bb4a650f9326f68e02c48
Leonid Freeman <lfreeman@realnetworks.com>
2016-04-20 23:45:09 -0400

    Replace Conditional with Polymorphism:

--- src/main/java/com/refactoring/NewReleasePrice.java
+++ src/main/java/com/refactoring/NewReleasePrice.java
@@ -7,4 +7,13 @@ public class NewReleasePrice extends Price {
         return Movie.NEW_RELEASE;
     }
 
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
 }
--- src/main/java/com/refactoring/Price.java
+++ src/main/java/com/refactoring/Price.java
@@ -9,26 +9,9 @@ public abstract class Price {
      * @param daysRented TODO
      * @return
      */
-    double getCharge(int daysRented) {
-        double result = 0;
-        // determine amounts for each line
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
